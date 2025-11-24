2b7356094ec351698ecc34a469aaaaf63c8dbd12
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-11-24 03:26:06 -0500

    feat: agregados puertos IN y OUT para gestionar Universidades

---
+++ src/main/java/com/jcaa/universidad_hexagonal/core/port/in/CreateUniversityUseCase.java
@@ -0,0 +1,4 @@
+package com.jcaa.universidad_hexagonal.core.port.in;
+
+public class CreateUniversityUseCase {
+}
---
+++ src/main/java/com/jcaa/universidad_hexagonal/core/port/out/UniversityRepositoryPort.java
@@ -0,0 +1,4 @@
+package com.jcaa.universidad_hexagonal.core.port.out;
+
+public class UniversityRepositoryPort {
+}
