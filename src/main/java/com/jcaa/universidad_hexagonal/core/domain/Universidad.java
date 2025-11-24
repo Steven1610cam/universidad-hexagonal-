49d62f86e5ac2da625201c43989307fede5e784e
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-11-24 03:19:37 -0500

    feat: Entidad Universidad agregada en el dominio

---
+++ src/main/java/com/jcaa/universidad_hexagonal/core/domain/Universidad.java
@@ -0,0 +1,15 @@
+package com.jcaa.universidad_hexagonal.core.domain;
+
+public class Universidad {
+    private Long id;
+    private String nombre;
+    private String categoria;
+    private String web;
+    private String rector;
+    private String email;
+    private String acceso;
+    private String telefono;
+    private String ciudad;
+    private Integer numeroCarreras;
+    private Integer numSedes;
+}
