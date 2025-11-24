ca83905d46f1a38ede93bde0b610b6ecbbf3c297
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-11-24 03:11:10 -0500

    configuración inicial del proyecto: H2, application.properties y estructura hexagonal

---
+++ src/main/java/com/jcaa/universidad_hexagonal/UniversidadHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.jcaa.universidad_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversidadHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcaa/universidad_hexagonal/UniversidadHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaa.universidad_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
